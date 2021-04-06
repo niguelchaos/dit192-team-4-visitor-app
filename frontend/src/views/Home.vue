d2391a7532273819a0ed1448412d0fce05f48360
Nickeboy <n.paasonen@yahoo.com>
2021-04-06 12:25:31 +0200

    test push

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <b-jumbotron header="Hello World" lead="Welcome to Your Frontend Vue.js App">
+    <b-jumbotron header="Hello World" lead="Welcome our amusement park">
       <p>Message from the server: {{ message }}</p>
       <b-button variant="primary" href="/camels">List Camels</b-button>
     </b-jumbotron>
     <b-container>
       <b-row>
-        <b-col><h1>Responsive BootstrapVue: <a href="https://bootstrap-vue.js.org/docs/components/layout/">Layout and Grid System</a></h1></b-col>
+        <b-col>
+          <h1>
+            Responsive BootstrapVue:
+            <a href="https://bootstrap-vue.js.org/docs/components/layout/">Layout and Grid System</a>
+          </h1>
+        </b-col>
       </b-row>
       <!-- Mobile (< 768px): stack columns by making one half-width and one full-width
-           Desktop (>= 768px): keep both columns in same row in 4:8 ratio -->
+      Desktop (>= 768px): keep both columns in same row in 4:8 ratio-->
       <b-row>
         <b-col class="example-col" cols="6" md="4">Message from the server</b-col>
         <b-col class="example-col" cols="12" md="8">{{ message }}</b-col>
@@ -20,36 +25,36 @@
 
 <script>
 // @ is an alias to /src
-import { Api } from '@/Api'
+import { Api } from "@/Api";
 
 export default {
-  name: 'home',
+  name: "home",
   data() {
     return {
-      message: ''
-    }
+      message: ""
+    };
   },
   mounted() {
-    this.getMessage()
+    this.getMessage();
   },
   methods: {
     getMessage() {
-      Api.get('/')
+      Api.get("/")
         .then(response => {
-          this.message = response.data.message
+          this.message = response.data.message;
         })
         .catch(error => {
-          this.message = error
-        })
+          this.message = error;
+        });
     }
   }
-}
+};
 </script>
 
 <style>
 .example-col {
-  padding-top: .75rem;
-  padding-bottom: .75rem;
+  padding-top: 0.75rem;
+  padding-bottom: 0.75rem;
   background-color: whitesmoke;
   border: 2px solid lightgray;
 }
