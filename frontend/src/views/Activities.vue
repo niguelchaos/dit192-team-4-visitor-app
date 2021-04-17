acc7ca87742d6636e174ef94553d525d709af31b
niguelchaos <niguelchaos@gmail.com>
2021-04-17 16:58:16 +0200

    began work on frontend card, unable to get padding and centering stuff. Vue should burn in trash.

--- frontend/src/views/Activities.vue
+++ frontend/src/views/Activities.vue
@@ -12,8 +12,21 @@
         <b-col>
           <h2 class="title">Activities</h2>
           <!-- Load attractions via API -->
+          <b-card class="act-card overflow-hidden rounded" img-right no-body>
+            <b-row>
+              <b-col xs="7" no-gutters class="card-col">
+                <div class="card-title"><span>Super Ride</span></div>
+                <!-- <b-card-body title="Super Ride"></b-card-body> -->
+                <b-link href="#" class="card-link">Read More</b-link>
+              </b-col>
+              <b-col xs="5">
+                <b-card-img src="https://picsum.photos/400/400/?image=20" alt="Image" class="card-image rounded-3"></b-card-img>
+              </b-col>
+            </b-row>
+          </b-card>
+
           <ul>
-            <li v-for="a in attractions" v-bind:key="a">
+            <li v-for="a in attractions" v-bind:key="a.id">
               {{ a.name + ": " + a.description }}
             </li>
           </ul>
@@ -51,4 +64,25 @@ export default {
 </script>
 
 <style>
+.act-card {
+  padding: 5%;
+}
+.card-col {
+  align-content: center;
+  padding: 0%;
+  margin: 0% ;
+}
+.card-title {
+  padding: 5%;
+  align-content: center;
+}
+/* .act-card-body {
+  padding: 0%;
+  align-content: center;
+} */
+/* .act-card-text {
+  padding: 0%;
+  align-content: center;
+} */
+
 </style>
