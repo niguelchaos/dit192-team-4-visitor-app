89618a2ddd870d11c4de51624673e212d20854d4
shilpa86 <vejella@student.chalmers.se>
2021-04-26 15:45:13 +0200

    Fixed the bugs with spaces

--- frontend/src/views/SingleTicket.vue
+++ frontend/src/views/SingleTicket.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
     <b-container>
-       <b-row>
-         <b-col>
-      <h1 class="title">Single Ticket</h1>
-         </b-col></b-row>
-          <b-row>
-          <b-col>
-            <p>Your ticket to access to a single activity once. Select type of activity, age and book.</p>
-            <p>Note: You need entrance ticket also.</p>
-            </b-col>
-            </b-row>
+      <b-row>
+        <b-col>
+          <h1 class="title">Single Ticket</h1>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <p>Your ticket to access to a single activity once. Select type of activity, age and book.</p>
+          <p>Note: You need entrance ticket also.</p>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
