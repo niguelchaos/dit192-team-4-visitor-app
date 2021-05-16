bcbb27de458f01afec44219ae5c69cd4bc717eb9
selomie <selomie@student.chalmers.se>
2021-05-16 14:06:42 +0200

    Updated list

--- frontend/src/views/GetReservation.vue
+++ frontend/src/views/GetReservation.vue
@@ -10,15 +10,6 @@
       <div class="card" style="margin-left: 710px;margin-right: 710px;border-radius: 10px;">
         <div class="card-body">
           <h4 class="card-title" style="font-size: 1.5rem;">Timeslot 0</h4>
-          <font-awesome-icon style="color:pink;width: 20px; height: 20px;" :icon="['fas','clock']" ></font-awesome-icon>  1:00 - 2:30 pm
-          <a class="btn btn-primary bg-success mb-3" type="submit">Choose</a>
-          <p class="align-middle">Left Seat 2/50</p>
-        </div>
-      </div>
-      <br />
-      <div class="card" style="margin-left: 710px;margin-right: 710px;border-radius: 10px;">
-        <div class="card-body">
-          <h4 class="card-title" style="font-size: 1.5rem;">Timeslot 3</h4>
           <font-awesome-icon style="color:pink;width: 20px; height: 20px;" :icon="['fas','clock']"></font-awesome-icon> 3:00 - 4:30 pm
           <span style="color:" class="align-left font-weight-bolder">
             <a v-b-modal.modal-1 class="btn btn-primary bg-success mb-3" type="submit">Choose</a>
