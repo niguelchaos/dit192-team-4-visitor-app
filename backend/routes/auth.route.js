66da0d22548c5cceb6c864380e0ad6c0814e618d
Nickeboy <n.paasonen@yahoo.com>
2021-05-17 22:41:59 +0200

    added auth to routes again

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -7,9 +7,9 @@ router.post('/auth/login', authController.login);
 router.post('/auth/register', authController.register);
 router.get('/auth/me', authService.verifyToken, authController.me);
 router.delete('/auth/deregister/:id', authService.verifyToken, authService.authorizeSame(), authController.deregister);
-router.put('/auth/reservation', authController.addReservation);
-router.get('/auth/reservations', authController.getReservations);
-router.put('/auth/ticket', authController.addTicket);
-router.get('/auth/tickets', authController.getTickets);
+router.put('/auth/reservation', authService.verifyToken, authController.addReservation);
+router.get('/auth/reservations', authService.verifyToken, authController.getReservations);
+router.put('/auth/ticket', authService.verifyToken, authController.addTicket);
+router.get('/auth/tickets', authService.verifyToken, authController.getTickets);
 
 module.exports = router;
