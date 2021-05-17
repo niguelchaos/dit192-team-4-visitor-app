580201d2097c6796ccc4c182e6015f485ba7920b
shilpa86 <vejella@student.chalmers.se>
2021-05-17 15:55:50 +0200

    fixing the tabs in tickets page

--- frontend/src/views/Tickets.vue
+++ frontend/src/views/Tickets.vue
@@ -14,14 +14,14 @@
         <div>
           <b-card no-body>
           <b-tabs pills card align="center">
-          <b-tab title="Buy Tickets" active><b-card-text><ul class="tab1"></ul>
+          <b-tab class="tab1" title="Buy Tickets" active><b-card-text><ul>
           <b-button pill to= "/ticketprices/entrance" class="btnentrance">Entrance</b-button>
           <b-button pill to="/ticketprices/singleticket" class="btnST">Single Ticket</b-button>
           <br />
           <b-button pill to="/ticketprices/bundleticket" class="btnBT">Bundle Ticket</b-button>
-          <b-button pill to="/ticketprices/fullpackage" class="btnFP">Full Package</b-button>
+          <b-button pill to="/ticketprices/fullpackage" class="btnFP">Full Package</b-button></ul>
           </b-card-text></b-tab>
-          <b-tab title="My Tickets"><b-card-text>Tab contents 2</b-card-text></b-tab>
+          <b-tab class="tab2" title="My Tickets"><b-card-text>tab2 contents</b-card-text></b-tab>
           </b-tabs>
           </b-card>
         </div>
@@ -41,68 +41,33 @@
 </template>
 
 <style>
-.wrapper {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-}
-.mobile-categorybuttons {
-  background-color: #f6fff3;
-  border-radius: 35px;
-  padding: 2%;
-  font-size: 17px;
-}
-.pc-categorybuttons {
-  background-color: #ffffff;
-  border-radius: 40px;
-  padding: 1%;
-  font-size: 20px;
-}
-
-.flt-active {
- background-color: #EDADC7;
-}
-
-.flt-not-active{
-  background-color: #FFFFFF;
-}
-
 .tab1 {
-  background-color: #EDADC7 !important;
+  background: #c7efcf;
   border-radius: 25px;
   color: black;
   font-weight: bolder;
+  margin-left: 0%;
+  margin-right: 0%;
+  border-radius: 0px;
+  height: 9.5rem;
 }
 
 .tab2 {
-  background-color: #EDADC7 !important;
+  background: #c7efcf;
   border-radius: 25px;
   color: black;
   font-weight: bolder;
+  margin-left: 0%;
+  margin-right: 0%;
+  border-radius: 0px;
+  height: 9.5rem;
 }
 
-.card1-not-active{
-  background-color: #f6fff3 !important;
-  border-radius: 25px;
-  color: #2D3E4F;
-  font-weight: normal;
-}
-
-.card-main-div {
-  margin: 0%;
-  padding: 0%;
-  overflow-y: scroll;
-  position: relative;
-  flex: 1;
-}
 .card-main-col {
   margin: 0%;
   padding: 0%;
 }
-.page-bar-div {
-  height: 55px;
-  padding: 0px 20px;
-}
+
 .btnentrance {
   background-color: #FFFFFF !important;
   border: 10px;
