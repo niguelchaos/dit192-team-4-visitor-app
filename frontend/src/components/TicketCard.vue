add507aa4ab819e2590c6d8ec1513b4c310fdf3f
Nickeboy <n.paasonen@yahoo.com>
2021-05-17 17:04:58 +0200

    removed auth on ticket n reserv api calls

---
+++ frontend/src/components/TicketCard.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+      <b-card
+        class="res-card overflow-hidden text-left"
+        img-right
+        no-body
+        v-b-toggle="'reservation-details-' + ticket.name"
+      >
+        <b-col class="card-img-col">
+          <b-card-img
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/58557/ticket-emoji-clipart-md.png"
+            alt="Image"
+            height="90%"
+            width="100%"
+            class="card-image"
+          ></b-card-img>
+        </b-col>
+
+        <b-col
+          style="font-size: 0.75rem;"
+          no-gutters
+          class="card-text-col"
+        >
+          <b-card-title style="font-size: 1rem;" class="card-title">
+            <span class="align-middle font-weight-bolder">{{
+              ticket.name
+            }}</span>
+          </b-card-title>
+          <p class="card-queue-text card-text mt-3">Queue: 30 min</p>
+        </b-col>
+
+      </b-card>
+  </div>
+</template>
+
+<script>
+// this stupid camel case props thing i dont get it
+export default {
+  name: 'TicketCard',
+  props: ['ticket']
+}
+</script>
+
+<style scoped>
+.res-card {
+   padding: 5%;
+   margin: 3%;
+   border-radius: 15px;
+   /* height: 190px; */
+}
+.card-img-col {
+  margin:0%;
+  padding:0%;
+}
+.card-text-col {
+  color: #2d3e4f;
+  padding: 1%;
+  padding-right: 3%;
+  margin: 0%;
+}
+.card-image {
+  border-radius: 15px;
+}
+.card-title {
+  padding-top: 15%;
+  padding-bottom: 1%;
+  align-content: center;
+}
+.card-queue-text {
+  padding: 0;
+}
+.card-link {
+  color: #e28daf;
+}
+.btnrese{
+  float: right;
+  height:35px;
+  width:88px;
+  right: 10%;
+  background-color: #388659;
+}
+</style>
